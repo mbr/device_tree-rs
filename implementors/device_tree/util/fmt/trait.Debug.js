9d0cae35f3d901113488f379a071794cc930a067
Marc Brinkmann <git@marcbrinkmann.de>
2016-03-20 01:18:57 +0100

    Automatic commmit using gittar.

---
+++ implementors/device_tree/util/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['device_tree'] = ["impl <a class='trait' href='device_tree/util/fmt/trait.Debug.html' title='device_tree::util::fmt::Debug'>Debug</a> for <a class='enum' href='device_tree/util/enum.SliceReadError.html' title='device_tree::util::SliceReadError'>SliceReadError</a>","impl <a class='trait' href='device_tree/util/fmt/trait.Debug.html' title='device_tree::util::fmt::Debug'>Debug</a> for <a class='enum' href='device_tree/enum.DeviceTreeError.html' title='device_tree::DeviceTreeError'>DeviceTreeError</a>","impl <a class='trait' href='device_tree/util/fmt/trait.Debug.html' title='device_tree::util::fmt::Debug'>Debug</a> for <a class='struct' href='device_tree/struct.DeviceTree.html' title='device_tree::DeviceTree'>DeviceTree</a>","impl <a class='trait' href='device_tree/util/fmt/trait.Debug.html' title='device_tree::util::fmt::Debug'>Debug</a> for <a class='struct' href='device_tree/struct.Node.html' title='device_tree::Node'>Node</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
