823d8c28c28f79ea2b77c5d357d91ef02ecd96bc
Marc Brinkmann <git@marcbrinkmann.de>
2016-06-15 13:12:41 +0200

    Automatic commmit using gittar.

---
+++ device_tree/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["DeviceTreeError","An error describe parsing problems when creating device trees."],["PropError",""]],"mod":[["util",""]],"struct":[["DeviceTree","Device tree structure."],["Node","A single node in the device tree."]]});
---
+++ implementors/device_tree/util/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['device_tree'] = ["impl <a class='trait' href='device_tree/util/convert/trait.From.html' title='device_tree::util::convert::From'>From</a>&lt;<a class='enum' href='device_tree/util/enum.SliceReadError.html' title='device_tree::util::SliceReadError'>SliceReadError</a>&gt; for <a class='enum' href='device_tree/enum.DeviceTreeError.html' title='device_tree::DeviceTreeError'>DeviceTreeError</a>","impl <a class='trait' href='device_tree/util/convert/trait.From.html' title='device_tree::util::convert::From'>From</a>&lt;<a class='struct' href='device_tree/util/str/struct.Utf8Error.html' title='device_tree::util::str::Utf8Error'>Utf8Error</a>&gt; for <a class='enum' href='device_tree/enum.DeviceTreeError.html' title='device_tree::DeviceTreeError'>DeviceTreeError</a>","impl <a class='trait' href='device_tree/util/convert/trait.From.html' title='device_tree::util::convert::From'>From</a>&lt;<a class='struct' href='device_tree/util/str/struct.Utf8Error.html' title='device_tree::util::str::Utf8Error'>Utf8Error</a>&gt; for <a class='enum' href='device_tree/enum.PropError.html' title='device_tree::PropError'>PropError</a>","impl <a class='trait' href='device_tree/util/convert/trait.From.html' title='device_tree::util::convert::From'>From</a>&lt;<a class='enum' href='device_tree/util/enum.SliceReadError.html' title='device_tree::util::SliceReadError'>SliceReadError</a>&gt; for <a class='enum' href='device_tree/enum.PropError.html' title='device_tree::PropError'>PropError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
