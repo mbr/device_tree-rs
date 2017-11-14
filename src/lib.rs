f1beb2a568f1aafb0963f1eb329a63daac20226d
Steven Walter <swalter@lexmark.com>
2017-11-14 13:29:53 -0500

    Implement optional support for deduplicating the strings section
    This support is optional since it uses HashMap from libstd

--- src/lib.rs
+++ src/lib.rs
@@ -130,24 +130,63 @@ impl From<str::Utf8Error> for DeviceTreeError {
     }
 }
 
-pub struct StringTable {
-    buffer: Vec<u8>,
-}
+#[cfg(feature = "string-dedup")]
+mod advancedstringtable {
+    use std::collections::HashMap;
 
-impl StringTable {
-    pub fn new() -> StringTable {
-        StringTable {
-            buffer: Vec::new(),
+    pub struct StringTable {
+        pub buffer: Vec<u8>,
+        index: HashMap<String, u32>,
+    }
+
+    impl StringTable {
+        pub fn new() -> StringTable {
+            StringTable {
+                buffer: Vec::new(),
+                index: HashMap::new(),
+            }
+        }
+
+        pub fn add_string(&mut self, val: &str) -> u32 {
+            if let Some(offset) = self.index.get(val) {
+                return *offset;
+            }
+            let offset = self.buffer.len() as u32;
+            self.buffer.extend(val.bytes());
+            self.buffer.push(0);
+            self.index.insert(val.to_string(), offset);
+            offset
         }
     }
+}
 
-    pub fn add_string(&mut self, val: &str) -> u32 {
-        let offset = self.buffer.len();
-        self.buffer.extend(val.bytes());
-        self.buffer.push(0);
-        offset as u32
+#[cfg(not(feature = "string-dedup"))]
+mod stringtable {
+    pub struct StringTable {
+        pub buffer: Vec<u8>,
+    }
+
+    impl StringTable {
+        pub fn new() -> StringTable {
+            StringTable {
+                buffer: Vec::new(),
+            }
+        }
+
+        pub fn add_string(&mut self, val: &str) -> u32 {
+            let offset = self.buffer.len();
+            self.buffer.extend(val.bytes());
+            self.buffer.push(0);
+            offset as u32
+        }
     }
 }
+
+#[cfg(not(feature = "string-dedup"))]
+use stringtable::StringTable;
+
+#[cfg(feature = "string-dedup")]
+use advancedstringtable::StringTable;
 
 impl DeviceTree {
     //! Load a device tree from a memory buffer.
